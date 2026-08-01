58db943b82da3479137db259b9c694d841adeae4
nitiiinn <nitin21kothiyal@gmail.com>
2026-08-02 01:52:54 +0530

    data types

--- HELLO.js
+++ HELLO.js
@@ -1,1 +1,22 @@
 console.log("hello")
+const ID=7
+let username="stark"
+var password="12345"
+city="delhi"
+let country
+
+// ID=21
+// prefer using let isntead of var since scope of var is not defined 
+// for non value variables , value is undefined
+
+username="tony"
+password="78910"
+
+
+console.log(ID);
+console.log(username);
+console.log(password);
+
+
+console.table([ID,username,password,city,country])
+
